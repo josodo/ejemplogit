ab4e15ecee317569a7f2ad7c9afb2a3015c7a9f6
jlsoler <jlsoler@SINF9.florida.local>
2013-04-24 17:47:51 +0200

    proba666

---
+++ Pr3/src/paquete1/Probando123.java
@@ -0,0 +1,14 @@
+package paquete1;
+
+public class Probando123 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hola mundo");
+
+	}
+
+}
